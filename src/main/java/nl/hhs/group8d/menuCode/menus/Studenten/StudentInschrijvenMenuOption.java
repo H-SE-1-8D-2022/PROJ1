231279833526a27b94eec403fcf3bdabf9f4f11f
FenderRoy <visser.roy@mail.com>
2022-03-31 23:00:21 +0200

    temp

--- src/main/java/nl/hhs/group8d/menuCode/menus/Studenten/StudentInschrijvenMenuOption.java
+++ src/main/java/nl/hhs/group8d/menuCode/menus/Studenten/StudentInschrijvenMenuOption.java
@@ -2,7 +2,7 @@ package nl.hhs.group8d.menuCode.menus.Studenten;
 
 import nl.hhs.group8d.menuCode.MenuOption;
 
-public class StudentInschrijvenMenuOption extends MenuOption {
+public class StudentInschrijvenMenuOption implements MenuOption {
     @Override
     public String getTitle() {
         return "Student inschrijven.";
--- src/main/java/nl/hhs/group8d/menuCode/menus/Studenten/StudentVerwijderenMenuOption.java
+++ src/main/java/nl/hhs/group8d/menuCode/menus/Studenten/StudentVerwijderenMenuOption.java
@@ -2,7 +2,7 @@ package nl.hhs.group8d.menuCode.menus.Studenten;
 
 import nl.hhs.group8d.menuCode.MenuOption;
 
-public class StudentVerwijderenMenuOption extends MenuOption {
+public class StudentVerwijderenMenuOption implements MenuOption {
     @Override
     public String getTitle() {
         return "Verwijder een student.";
