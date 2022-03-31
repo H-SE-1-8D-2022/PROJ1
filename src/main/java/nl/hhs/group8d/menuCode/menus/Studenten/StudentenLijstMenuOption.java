b0591180dc9c655c96aa8558d2d932b5a56b1275
FenderRoy <visser.roy@mail.com>
2022-03-31 22:43:04 +0200

    studentenlijstmenuoption added

--- src/main/java/nl/hhs/group8d/menuCode/menus/Studenten/StudentenLijstMenuOption.java
+++ src/main/java/nl/hhs/group8d/menuCode/menus/Studenten/StudentenLijstMenuOption.java
@@ -5,16 +5,81 @@ import nl.hhs.group8d.menuCode.iMenuOption;
 import nl.hhs.group8d.menuCode.menus.Examen.AfnemenExamenMenuOption;
 import nl.hhs.group8d.menuCode.menus.ExamenMenuOption;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class StudentenLijstMenuOption implements iMenuOption {
     @Override
     public String getTitle() {
         return "Lijst met Studenten.";
     }
 
+    private ArrayList<Student> studentenLijst;
+
     @Override
     public void executeMenuOption(){
 
 
+//        this.studentenLijst = getStudentenLijst();
+//
+//        printStudenten();
+//
+//        processInput(getUserInput());
 
     }
+
+    private ArrayList<Student> getStudentenLijst(){
+        //return Student.getStudentenLijst();
+        return null;
+    }
+
+    private void processInput(int input){
+        if(input == 0){
+            return;
+        } else {
+            printStudent(input - 1);
+        }
+    }
+
+    private int getUserInput(){
+        Boolean noValidInput = true;
+        while(noValidInput) {
+            try {
+                int index = getScanner().nextInt();
+                if(index < 0 || index > studentenLijst.size()){
+                    System.out.println("Please enter a valid input.");
+                } else {
+                    return getScanner().nextInt();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid number.");
+            }
+        }
+
+        return 0;
+    }
+
+    private void printStudenten(){
+
+        int i = 1;
+        for(Student student : this.studentenLijst){
+            System.out.print(i+++". ");
+            System.out.print(student.getstudentNummer());
+            System.out.println(" ("+student.getNaam()+")");
+        }
+
+        System.out.println("0. Exit");
+
+    }
+
+    private void printStudent(int index){
+        Student student = studentenLijst.get(index);
+
+        System.out.println(student.getNaam());
+        System.out.println(student.getstudentNummer());
+        System.out.println("0. exit");
+        getScanner().nextLine();
+    }
+
 }
