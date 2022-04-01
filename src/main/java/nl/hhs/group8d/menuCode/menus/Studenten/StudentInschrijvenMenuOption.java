6f05db6f0385a7e956abbce3d98c20ef525116be
StephanKragten <101334492+StephanKragten@users.noreply.github.com>
2022-04-01 15:13:43 +0200

    StudentInschrijven & StudentVerwijderen toegevoegd

--- src/main/java/nl/hhs/group8d/menuCode/menus/Studenten/StudentInschrijvenMenuOption.java
+++ src/main/java/nl/hhs/group8d/menuCode/menus/Studenten/StudentInschrijvenMenuOption.java
@@ -2,6 +2,11 @@ package nl.hhs.group8d.menuCode.menus.Studenten;
 
 import nl.hhs.group8d.menuCode.MenuOption;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+
 public class StudentInschrijvenMenuOption extends MenuOption {
     @Override
     public String getTitle() {
@@ -10,6 +15,41 @@ public class StudentInschrijvenMenuOption extends MenuOption {
 
     @Override
     public void executeMenuOption() {
+        vraagStudentInfo();
+    }
 
+    private void vraagStudentInfo() {
+        System.out.println("Wat is je naam?");
+        String naam = getUserStringInput();
+        System.out.println("Wat is je studentennummer?");
+        int studentNummer = getUserIntInput(0, 99999999);
+        boolean geldigStudentNummer = true;
+        for(Student student : Student.studentenLijst){
+            if(student.getstudentNummer() == studentNummer){
+                geldigStudentNummer = false;
+            }
+        }
+        if(geldigStudentNummer){
+            inschrijvenStudent(naam, studentNummer);
+        }
+        else{
+            System.out.println("Studentnummer al in het systeem.");
+        }
+    }
+
+
+    private void inschrijvenStudent(String naam, int studentNummer){
+        Student student = new Student(naam, studentNummer);
+        Student.studentenLijst.add(student);
+        try(FileWriter fw = new FileWriter(Student.BESTAND, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw)){
+            pw.println(naam + "," +studentNummer);
+            System.out.println("Student account aangemaakt");
+            System.out.println("0. exit");
+            getUserStringInput();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/nl/hhs/group8d/menuCode/menus/Studenten/StudentVerwijderenMenuOption.java
+++ src/main/java/nl/hhs/group8d/menuCode/menus/Studenten/StudentVerwijderenMenuOption.java
@@ -1,6 +1,9 @@
 package nl.hhs.group8d.menuCode.menus.Studenten;
 
 import nl.hhs.group8d.menuCode.MenuOption;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class StudentVerwijderenMenuOption extends MenuOption {
     @Override
@@ -10,6 +13,45 @@ public class StudentVerwijderenMenuOption extends MenuOption {
 
     @Override
     public void executeMenuOption() {
+        vraagStudentnummer();
+    }
 
+    private void vraagStudentnummer() {
+        if(!checkVoorStudentnr()){
+            System.out.println("Dit studentnummer staat niet in het systeem!");
+            vraagStudentnummer();
+        }
+    }
+
+    private boolean checkVoorStudentnr(){
+        System.out.println("Vul studentnummer in: ");
+        int studentNummer = getUserIntInput(0, 99999999);
+        for (Student st : Student.studentenLijst) {
+            if (st.getstudentNummer() == studentNummer) {
+                verwijderStudent(st);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void verwijderStudent(Student st){
+        Student.studentenLijst.remove(st);
+        try(PrintWriter writer = new PrintWriter(Student.BESTAND))
+        {writer.print("");        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for(Student stud : Student.studentenLijst) {
+            try (FileWriter fw = new FileWriter(Student.BESTAND, true);
+                 BufferedWriter bw = new BufferedWriter(fw);
+                 PrintWriter pw = new PrintWriter(bw)) {
+                pw.println(stud.getNaam() + "," + stud.getstudentNummer());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Student: " + st.getNaam() + " met het nummer: " + st.getstudentNummer() +  " verwijderd.");
     }
 }
