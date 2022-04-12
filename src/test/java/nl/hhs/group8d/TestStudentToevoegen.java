8bfa578796f09b114ec0a34d96d3b9ac41cc2478
StephanKragten <21091145@student.hhs.nl>
2022-04-12 08:49:23 +0200

    Testen veranderd en student opgeschoond

--- src/test/java/nl/hhs/group8d/TestStudentToevoegen.java
+++ src/test/java/nl/hhs/group8d/TestStudentToevoegen.java
@@ -3,51 +3,43 @@ package nl.hhs.group8d;
 import nl.hhs.group8d.menuCode.menus.Studenten.Student;
 import nl.hhs.group8d.menuCode.menus.Studenten.StudentInschrijvenMenuOption;
 import nl.hhs.group8d.menuCode.menus.Studenten.StudentVerwijderenMenuOption;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
-public class TestStudentToevoegenEnVerwijderen {
+
+public class TestStudentToevoegen {
     @Test
     public void studentToevoegen(){
-
+        //Arrange
+        StudentInschrijvenMenuOption studentInschrijven = new StudentInschrijvenMenuOption();
         String input = "Stephan\n147852\n0\n";
         ByteArrayInputStream byteStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
         System.setIn(byteStream);
-
-        StudentInschrijvenMenuOption studentInschrijven = new StudentInschrijvenMenuOption();
-        studentInschrijven.executeMenuOption();
-
         Student expectedResult = new Student("Stephan", 147852);
 
+        //Act
+        studentInschrijven.executeMenuOption();
         Student actualResult = Student.studentenLijst.get(Student.studentenLijst.size() - 1);
 
+        //Assert
         assertEquals(expectedResult.getNaam(), actualResult.getNaam());
-
         assertEquals(expectedResult.getstudentNummer(), actualResult.getstudentNummer());
-
-
     }
-
-    @Test
-    public void studentVerwijderen(){
+    
+    @AfterEach
+    public void verijwderStudent(){
         String input = Student.studentenLijst.size() + "\n0\n";
         ByteArrayInputStream byteStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
         System.setIn(byteStream);
-
         StudentVerwijderenMenuOption studentVerwijderen = new StudentVerwijderenMenuOption();
-
         studentVerwijderen.executeMenuOption();
-
-        Student actualResult = Student.studentenLijst.get(Student.studentenLijst.size() - 1);
-
-        int expectedResult = 147852;
-        assertNotEquals(expectedResult, actualResult.getstudentNummer());
     }
+
 
 
 }
